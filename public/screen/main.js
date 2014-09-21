19cdf0116fa6ddd8e313188dd0f5b40c6e2bca33
zsx <zsxsoft@outlook.com>
2014-09-21 17:49:55 +0800

    socket.io interaction

--- public/screen/main.js
+++ public/screen/main.js
@@ -3,13 +3,14 @@ define(function(require, exports, module) {
 
 	var $ = require('jquery'),
 		program = require('screen/program'),
-		dom = require('screen/dom')
+		dom = require('screen/dom'),
+		socket = require('screen/socket');
 
 	$(function() {
 		program.objects.player = $("#dom-background-audio");
 		program.init({
 			socket: location.origin
-		}).register(dom);
+		}).register(dom).registerSocket(socket);
 
 		// debug
 		window.program = program;

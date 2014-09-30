083b8e4dccbbaab834be2d72430e45c3b3449998
zsxsoft <zsxsoft@outlook.com>
2014-09-30 13:05:49 +0800

    Switch page automatically and Add default page

--- public/screen/dom.js
+++ public/screen/dom.js
@@ -15,6 +15,9 @@ define(function(require, exports, module) {
 				slide_toggle(object, [{action: "absolute", pos: 0}]);
 				init_bgm(object, [{action: "absolute", pos: 0}]);
 
+				object.$(".dom-playing-name").text(object.program.program.name);
+				object.$(".dom-playing-player").text(object.program.player.class + " " + object.program.player.name);
+
 			}
 		},
 		{
@@ -46,7 +49,8 @@ define(function(require, exports, module) {
 
 	var auto_toggle = function(object) {
 
-		time = object.display[object.current.display]["time"];
+		if (object.display.length == 0) return;
+		var time = object.display[object.current.display]["time"];
 		if (time) {
 
 			var curTime = object.objects.player[0].currentTime, 
@@ -77,6 +81,21 @@ define(function(require, exports, module) {
 		$("#display-data .background-image").css("background-image", "");
 		var displayData = $("#display-data").html("");
 
+		if (!object.current.index) {
+			var fullscreen = $("<div>").addClass("cover-container")
+									   .css("background-color", "#333")
+									   .append($("<div>")
+									   	      .addClass("inner")
+									   	      .html("<h1 class=\"dom-playing-name title-name cover-heading\"></h1><code class=\"lead dom-playing-player title-player yahei-font\"></code>")
+									    );
+			$("<div>").addClass("display-child-data")
+					.attr("id", "display-block-0")
+					.attr("data-id", 0)
+					.attr("data-type", "default")
+					.append(fullscreen)
+					.appendTo(displayData);
+		}
+
 		$.each(object.display, function(i, o) {
 			var fullscreen = $("<div>").addClass("div-fullscreen");
 
@@ -97,7 +116,7 @@ define(function(require, exports, module) {
 
 			}
 
-			$("<div>").addClass("display-child-data")
+			$("<div>").addClass("display-child-data display-child-fullscreen")
 					//.addClass((i == 0 ? 'slide-current animate-fadeIn': ''))
 					.attr("id", "display-block-" + i)
 					.attr("data-id", i)
@@ -109,14 +128,18 @@ define(function(require, exports, module) {
 			o["minus_time"] = (i == 0 ? 1 : (o["time"] - object.display[i - 1]["time"]));
 			o["dom_id"] = i;
 
-		})
+		});
+
 
 	};
 
 
 	var slide_toggle = function(object, argu) {
 
+		if (object.display.length == 0) return;
+
 		var $ = object.$;
+
 
 		$(".animate-fadeIn").removeClass('animate-fadeIn');
 		$(".slide-prepareCurrent").removeClass('slide-prepareCurrent')
@@ -127,13 +150,11 @@ define(function(require, exports, module) {
 			thisSlideId = object.current.display,//thisSlide.data("id"),
 			nextSlideId = (argu[0].action == "absolute" ? argu[0].pos : thisSlideId + argu[0].pos);
 
-		console.log("this: " + thisSlideId + ",  next: " + nextSlideId + ",   pos:" + argu[0].pos);
 		if (nextSlideId >= object.display.length) {
 			nextSlideId %= (object.display.length);
 		} else if (nextSlideId == NaN) {
 			nextSlideId = 0;
 		}
-		console.log("this: " + thisSlideId + ",  next: " + nextSlideId + ",   pos:" + argu[0].pos);
 
 		var nextSlide = $("#display-block-" + object.display[nextSlideId].dom_id);
 

07b6e1a5b0fb78aec837ca7dad4f08fe287f6203
zsxsoft <zsxsoft@outlook.com>
2014-11-01 21:48:19 +0800

    中文语言�

---
+++ lang/zh-cn.js
@@ -0,0 +1,69 @@
+/*
+ * 我英语就是不行！
+ * 怎样！
+ * 反正你用这个的话肯定是中国人啦！
+ */
+exports.lang = {
+	global: {
+		controller: "控制台",
+		screen: "屏幕",
+		editor: "编辑器"
+	},
+	console: {
+		serverCreated: "服务器已经成功创建于%u% !",
+		requestCreated: "请求 (%s%) 已经创建：",
+		clientConnected: "新的%c% (%d%) 连接成功！",
+		requestFinish: "请求 (%d%) 收到响应：",
+		dataTransfered: "数据已传输到 (%d%)。",
+		runSql: "接收到待执行SQL语句： %s%",
+		sqlError: "SQL执行错误：%s%"
+	},
+	controller: {
+		prev: "向前",
+		prevSlide: "上一页",
+		prevMusic: "上一曲",
+		next: "向后",
+		nextSlide: "下一页",
+		nextMusic: "下一曲",
+		play: "播放",
+		pause: "暂停",
+		stop: "停止",
+		run: "运行",
+		refresh: "刷新",
+		javascript: "在这里输入JavaScript代码",
+		dashboard: "控制板",
+		information: "节目信息",
+		controlSlideMusic: "背景及音乐控制",
+		advanced: "高级",
+		playingId: "节目ID：",
+		playingName: "节目名：",
+		player: "表演者：",
+		programControl: "节目控制",
+		programSelect: "选择一个节目：",
+		musicSelect: "选择一个背景音乐：",
+		modeBackground: "背景模式",
+		modeScore: "评分模式",
+		
+	},
+	screen: {
+		footer: {
+			left: "This project created by zsx.",
+			right: 'GitHub: zsxsoft'
+		}
+	},
+	editor: {
+		information: "节目信息",
+		programId: "节目ID: ",
+		programSort: "节目排序: ",
+		programName: "节目名: ",
+		playerName: "表演者名: ",
+		playerClass: "表演者班级: ",
+		playerDoom: "表演者宿舍: ",
+		displayData: "背景显示管理",
+		backgroundMusic: "背景音乐管理",
+		score: "分数数据管理",
+
+		submit: "提交",
+	}
+
+};

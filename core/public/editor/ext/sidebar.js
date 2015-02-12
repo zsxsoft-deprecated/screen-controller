a59a59674c9303f52e6a78624751b508062fb0ad
zsx <zsx@zsxsoft.com>
2015-02-12 14:03:14 +0800

    Add "add" function

--- core/public/editor/ext/sidebar.js
+++ core/public/editor/ext/sidebar.js
@@ -28,13 +28,25 @@ define(function(require, exports, module) {
 				.attr("href", "#")
 				.html(v.program.name)
 				.appendTo($("<li>").addClass("item " + (highlightId == i ? "active" : "")).appendTo(dom));
-		})
+		});
+
+		$("<li>").addClass("item item-add").append($("<a>").html("Add..").attr("href", "#")).appendTo(dom);
 	}
 
 	var bindSideBar = function(dom) {
 		var me = this;
 		dom.on('click', '.item', function() {
-			me.toProgram($(this).children("a").data("aryId"));
+			var $a = $(this).children("a");
+			if ($(this).hasClass("item-add")) {
+				var copyArr = $.extend({}, me.programs[0]);
+				var ubPtr = me.programs.length;
+				me.programs.push(copyArr);
+				me.programs[ubPtr].sort = 32767;
+				me.programs[ubPtr].id = parseInt(me.programs[ubPtr - 1].id) + 1;
+				me.toProgram(ubPtr);
+			} else {
+				me.toProgram($a.data("aryId"));
+			}
 		}).data("bind", "bind");
 	}
 
